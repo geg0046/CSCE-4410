7b3f4044a73382ba91c1b263d38b7f104f012dfe
dillonstoutable <dillonstout@my.unt.edu>
2015-02-28 12:54:00 -0600

    quickfix
    removed statement preventing merge

--- app/src/main/java/comgeg0046/github/testapp/InGameLookup.java
+++ app/src/main/java/comgeg0046/github/testapp/InGameLookup.java
@@ -30,10 +30,11 @@ public class InGameLookup extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+       /* if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
+        //return true;
         return super.onOptionsItemSelected(item);
     }
 }
