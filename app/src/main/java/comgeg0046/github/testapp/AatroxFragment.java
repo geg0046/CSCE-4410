3ee8674daf364053d4fe14b5fb59c71436684f05
cweinberg21 <cweinberg21@gmail.com>
2015-04-28 16:06:09 -0500

    Automatically refreshes data retrieved from JSON  into Aatrox display:
    TO DO:
    Clean up Sunshine relic code
    Organize this functionality for all champions.

--- app/src/main/java/comgeg0046/github/testapp/AatroxFragment.java
+++ app/src/main/java/comgeg0046/github/testapp/AatroxFragment.java
@@ -1,7 +1,10 @@
 package comgeg0046.github.testapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,8 +27,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Callan on 4/6/2015.
@@ -55,8 +57,7 @@ public class AatroxFragment extends Fragment {
         //as you specify a parent activity in AndroidManifest.xml
         int id = item.getItemId();
         if(id == R.id.action_refresh){
-            FetchAatroxTask AatroxTask = new FetchAatroxTask();
-            AatroxTask.execute();
+            updateAatrox();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -67,16 +68,7 @@ public class AatroxFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
 
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
@@ -86,25 +78,40 @@ public class AatroxFragment extends Fragment {
                         getActivity(), // The current context (this activity)
                         R.layout.list_aatrox_items, // The name of the layout ID.
                         R.id.list_aatrox_items_textview, // The ID of the textview to populate.
-                        weekForecast);
+                        new ArrayList<String>());
 
         View rootView = inflater.inflate(R.layout.aatroxfragment_main, container, false);
 
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-        //LESSON 3 STUFF
-       /* listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
-
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
-        });*/
+        });
 
         return rootView;
+    }
+
+
+    private void updateAatrox() {
+        FetchAatroxTask AatroxTask = new FetchAatroxTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+            getString(R.string.pref_location_default));
+        AatroxTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateAatrox();
     }
 
     public class FetchAatroxTask extends AsyncTask<String, Void, String> {
