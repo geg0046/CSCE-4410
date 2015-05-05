10be92504fc454a7961a072ff6d9c9e160b015aa
cweinberg21 <cweinberg21@gmail.com>
2015-05-05 08:45:25 -0500

    ADDED: Spellview
    Polished up Statsview

--- app/src/main/java/comgeg0046/github/testapp/ChampionFragment.java
+++ app/src/main/java/comgeg0046/github/testapp/ChampionFragment.java
@@ -1,8 +1,13 @@
 package comgeg0046.github.testapp;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,6 +30,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+
 
 /**
  * Created by Callan on 4/6/2015.
@@ -77,20 +84,9 @@ public class ChampionFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.championfragment_main, container, false);
 
         // Get a reference to the ListView, and attach this adapter to it.
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_stats);
         listView.setAdapter(mChampionAdapter);
 
-        //uncomment this if we decide we want to have the stats clickable; otherwise KEEP THESE ~10 LINES COMMENTED
-        //listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-
-            //@Override
-            //public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
-            //    String forecast = mChampionAdapter.getItem(position);
-            //    Intent intent = new Intent(getActivity(), DetailActivity.class)
-            //            .putExtra(Intent.EXTRA_TEXT, forecast);
-            //    startActivity(intent);
-            //}
-        //});
 
         return rootView;
     }
@@ -107,6 +103,60 @@ public class ChampionFragment extends Fragment {
         updateChampion();
     }
 
+    /**
+     * Returns a CharSequence that concatenates the specified array of CharSequence
+     * objects and then applies a list of zero or more tags to the entire range.
+     *
+     * @param content an array of character sequences to apply a style to
+     * @param tags the styled span objects to apply to the content
+     *        such as android.text.style.StyleSpan
+     *
+     */
+    private static CharSequence apply(CharSequence[] content, Object... tags) {
+        SpannableStringBuilder text = new SpannableStringBuilder();
+        openTags(text, tags);
+        for (CharSequence item : content) {
+            text.append(item);
+        }
+        closeTags(text, tags);
+        return text;
+    }
+
+    /**
+     * Iterates over an array of tags and applies them to the beginning of the specified
+     * Spannable object so that future text appended to the text will have the styling
+     * applied to it. Do not call this method directly.
+     */
+    private static void openTags(Spannable text, Object[] tags) {
+        for (Object tag : tags) {
+            text.setSpan(tag, 0, 0, Spannable.SPAN_MARK_MARK);
+        }
+    }
+
+    /**
+     * "Closes" the specified tags on a Spannable by updating the spans to be
+     * endpoint-exclusive so that future text appended to the end will not take
+     * on the same styling. Do not call this method directly.
+     */
+    private static void closeTags(Spannable text, Object[] tags) {
+        int len = text.length();
+        for (Object tag : tags) {
+            if (len > 0) {
+                text.setSpan(tag, 0, len, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                text.removeSpan(tag);
+            }
+        }
+    }
+
+    /**
+     * Returns a CharSequence that applies a foreground color to the
+     * concatenation of the specified CharSequence objects.
+     */
+    public static CharSequence color(int color, CharSequence... content) {
+        return apply(content, new ForegroundColorSpan(color));
+    }
+
     public class FetchChampionTask extends AsyncTask<String, Void, String> {
 
         private final String LOG_TAG = FetchChampionTask.class.getSimpleName();
@@ -118,6 +168,8 @@ public class ChampionFragment extends Fragment {
             final String DD_DATA = "data";
             final String DD_INFO = "info";
             final String DD_STATS = "stats";
+            final String DD_PASSIVE = "passive";
+            final String DD_SPELLS = "spells";
             final String DD_NAME = championName;
             final String DD_ID = "id";
             final String DD_ATTACK = "attack";
@@ -148,15 +200,17 @@ public class ChampionFragment extends Fragment {
 
             //Champion "spells[4]"; Spells are inserted into an array, and read off by their
             final String DD_SNAME = "name";
-            final String DD_SDESCRIPTION = "description";
             final String DD_TOOLTIP = "tooltip"; //This might be worth using over the description, but will take some code to synergize with what e1/a1 mean, etc.
-            final String DD_MAXRANK = "maxrank";
-            final String DD_COOLDOWN = "cooldown"; //Array
-            final String DD_COST = "cost"; //Array
-            final String DD_EFFECT = "effect"; //Array
+            final String DD_EFFECTBURN = "effectBurn";
+            final String DD_COOLDOWNBURN = "cooldownBurn"; //Array
+            final String DD_COSTBURN = "costBurn"; //Array
             final String DD_VARS = "vars"; //Array; "link," "coeff," "key."
+                final String DD_LINK = "link";
+                final String DD_COEFF = "coeff";
+                final String DD_KEY = "key";
             final String DD_COSTTYPE = "costType";
-            final String DD_RANGE = "range"; //Array
+            final String DD_RANGEBURN = "rangeBurn"; //Array
+            final String DD_RESOURCE = "resource";
 
             //Champion "passive"
             final String DD_PDESCRIPTION = "description";
@@ -180,8 +234,8 @@ public class ChampionFragment extends Fragment {
             int difficulty = infoObject.getInt(DD_DIFFICULTY);
 
             //Lore
-            String lore = ChampionStats.getString(DD_LORE);
-            lore = lore.replace("<br>", "\n");
+           /* String lore = ChampionStats.getString(DD_LORE);
+            lore = lore.replace("<br>", "\n"); */
 
             //Stats
             JSONObject statsObject = ChampionStats.getJSONObject(DD_STATS);
@@ -211,10 +265,10 @@ public class ChampionFragment extends Fragment {
 
 
 
-            resultStrs = "Champion: " + name + "\nAttack: " + attack + " Defense: " + defense + " Magic: " + magic + " Difficulty: " + difficulty + "\n\n " +
-                         //"Lore: " + lore +
+            resultStrs = name + "\nGeneral Rating (out of 10):\n\nAttack: " + attack + " Defense: " + defense + " Magic: " + magic + " Difficulty: " + difficulty + "\n\n\n " +
 
-                    "\n\n\nHealth: " + hp + " (+" + hppl + " per level)\n" +
+                    "STATS:" +
+                    "\n\nHealth: " + hp + " (+" + hppl + " per level)\n" +
                     "Health Regen: " + hpr + " (+" + hprpl + " per level)\n" +
                     "Mana: " + mp + " (+" + mppl + " per level)\n" +
                     "Mana Regen: " + mpr + " (+" + mprpl + " per level)\n" +
@@ -223,7 +277,183 @@ public class ChampionFragment extends Fragment {
                     "Attack Range: " + ar + "\n" +
                     "Armor: " + armor + " (+" + armorpl + " per level)\n" +
                     "Magic Resistance: " + mr + " (+" + mrpl + " per level)\n" +
-                    "Movement Speed: " + movespd + "\n";
+                    "Movement Speed: " + movespd + "\n\n\n";
+
+
+            JSONObject passiveObject = ChampionStats.getJSONObject(DD_PASSIVE);
+            String passive = passiveObject.getString(DD_PDESCRIPTION);
+            resultStrs = resultStrs + "PASSIVE:\n\n" + passive + "\n\n\nABILITIES:\n\n";
+
+
+
+            JSONArray spellsArray = ChampionStats.getJSONArray(DD_SPELLS);
+                for(int i = 0; i < spellsArray.length(); i++){
+
+                JSONObject sDetails = spellsArray.getJSONObject(i);
+                String sName = sDetails.getString(DD_SNAME);
+                String sCostType = sDetails.getString(DD_COSTTYPE);
+                String sRange = sDetails.getString(DD_RANGEBURN);
+                String sCost = sDetails.getString(DD_COSTBURN);
+                String sCooldown = sDetails.getString(DD_COOLDOWNBURN);
+                String sResource = sDetails.getString(DD_RESOURCE);
+
+                JSONArray effectBurn = sDetails.getJSONArray(DD_EFFECTBURN);
+                int eBurnLength = effectBurn.length();
+
+                String[] eBurn = new String[eBurnLength];
+                for (int j = 1; j < eBurnLength; j++){
+                    eBurn[j] = effectBurn.getString(j);
+                }
+
+                resultStrs = resultStrs + sName + ":\n\n";
+
+                if (sResource.contains("{{ e"))
+                {
+                    for(int j = 1; j < effectBurn.length(); j++) {
+                        if (sResource.contains("{{ e" + j)) {
+                            int startIndex = sResource.indexOf("{{ e" + j);
+                            int endIndex = sResource.indexOf("e"+ j + " }}");
+                            String replacement = eBurn[j];
+                            String toBeReplaced = sResource.substring(startIndex, endIndex + 5);
+                            sResource = sResource.replace(toBeReplaced, replacement);
+                            resultStrs = resultStrs + sResource + "\n";
+                        }
+                    }
+                }
+                else{
+                    if(sCostType.equals("NoCost"))
+                    {
+                        resultStrs = resultStrs + "No Cost\n";
+                    }
+                    else {
+                        resultStrs = resultStrs + sCost + " " + sCostType + "\n";
+                    }
+                }
+
+                resultStrs = resultStrs + sCooldown + " second(s) cooldown\n" +
+                            sRange + " range\n\n";
+
+
+                String sTooltip = sDetails.getString(DD_TOOLTIP);
+                while(sTooltip.contains("<")){
+                    int startIndex = sTooltip.indexOf("<");
+                    int endIndex = sTooltip.indexOf(">");
+                    String replacement = "";
+                    String toBeReplaced = sTooltip.substring(startIndex, endIndex+1);
+                    sTooltip = sTooltip.replace(toBeReplaced, replacement);
+                }
+
+
+
+
+
+                for(int j = 1; j < effectBurn.length(); j++) {
+                    if (sTooltip.contains("{{ e" + j)) {
+                        int startIndex = sTooltip.indexOf("{{ e" + j);
+                        int endIndex = sTooltip.indexOf("e"+ j + " }}");
+                        String replacement = eBurn[j];
+                        String toBeReplaced = sTooltip.substring(startIndex, endIndex + 5);
+                        sTooltip = sTooltip.replace(toBeReplaced, replacement);
+
+                    }
+                }
+
+                JSONArray vars = sDetails.getJSONArray(DD_VARS);
+                if(!vars.equals(null)) {
+                    String[] coeff = new String[vars.length() + 1];
+                    String[] coefff = new String[vars.length() + 1];
+
+                    for (int j = 0; j < vars.length(); j++) {
+                        JSONObject var = vars.getJSONObject(j);
+                        String coeffa = var.getString(DD_COEFF);
+                        String key = var.getString(DD_KEY);
+                        String link = var.getString(DD_LINK);
+                        if (link.equals("bonusattackdamage"))
+                            link = "Bonus AD";
+                        else
+                        if (link.equals("spelldamage"))
+                            link = "AP";
+                        else
+                        if (link.equals("attackdamage"))
+                            link = "AD";
+                        else
+                        if (link.equals("@cooldownchampion"))
+                            link = " ";
+                        else
+                        if (link.equals("@dynamic.abilitypower"))
+                            link = "AP Ratio";
+                        if(key.contains("a")) {
+                            key = key.replace("a", "");
+                            int index = Integer.parseInt(key);
+                            coeff[index] = coeffa + " " + link;
+                        }
+                        else
+                        if(key.contains("f")) {
+                            key = key.replace("f", "");
+                            int index = Integer.parseInt(key);
+                            coefff[index] = coeffa + " " + link;
+                        }
+                    }
+
+
+                    for (int j = 1; j < vars.length() + 1; j++) {
+
+                        if (sTooltip.contains("{{ a" + j)) {
+
+                            //for(int k = 0; k < vars.length(); k++) {
+                            //JSONObject eleVar = vars.getJSONObject(k);
+                            //String key = eleVar.getString(DD_KEY);
+
+
+                            String replacement = coeff[j];
+                            int startIndex = sTooltip.indexOf("{{ a" + j);
+                            int endIndex = sTooltip.indexOf("a" + j + " }}");
+                            String toBeReplaced = sTooltip.substring(startIndex, endIndex + 5);
+                            sTooltip = sTooltip.replace(toBeReplaced, replacement);
+
+
+                            //}
+                        }
+                        if (sTooltip.contains("{{ f" + j)) {
+
+                            //for(int k = 0; k < vars.length(); k++) {
+                            //JSONObject eleVar = vars.getJSONObject(k);
+                            //String key = eleVar.getString(DD_KEY);
+                            if(coefff[j] != null) {
+                                String replacement = coefff[j];
+                                int startIndex = sTooltip.indexOf("{{ f" + j);
+                                int endIndex = sTooltip.indexOf("f" + j + " }}");
+                                String toBeReplaced = sTooltip.substring(startIndex, endIndex + 5);
+                                sTooltip = sTooltip.replace(toBeReplaced, replacement);
+                            }
+                            else
+                            {
+                                String replacement = "";
+                                int startIndex = sTooltip.indexOf("{{ f" + j);
+                                int endIndex = sTooltip.indexOf("f" + j + " }}");
+                                String toBeReplaced = sTooltip.substring(startIndex, endIndex + 5);
+                                sTooltip = sTooltip.replace(toBeReplaced, replacement);
+                            }
+                        }
+
+
+                            //}
+
+                    }
+                }
+
+                if (sTooltip.contains("{")) {
+                    while (sTooltip.contains("{")) {
+                        String replacement = "";
+                        int startIndex = sTooltip.lastIndexOf("{");
+                        int endIndex = sTooltip.lastIndexOf("}");
+                        String toBeReplaced = sTooltip.substring(startIndex-1, endIndex + 1);
+                        sTooltip = sTooltip.replace(toBeReplaced, replacement);
+                    }
+                }
+                resultStrs = resultStrs + sTooltip + "\n\n\n";
+
+            }
 
 
 
